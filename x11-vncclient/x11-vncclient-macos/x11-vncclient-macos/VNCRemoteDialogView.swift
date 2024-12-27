85aa5557c2fd8a4d1af22416dc291a147c1922eb
Victor Liu <ilhanwnz@hotmail.com>
2024-12-27 23:09:43 +0800

    Add a host info dialog and a remote screen view

---
+++ x11-vncclient/x11-vncclient-macos/x11-vncclient-macos/VNCRemoteDialogView.swift
@@ -0,0 +1,8 @@
+//
+//  VNCRemoteDialogView.swift
+//  x11-vncclient-macos
+//
+//  Created by Yu Liu on 2024/12/27.
+//
+
+import Foundation
---
+++ x11-vncclient/x11-vncclient-macos/x11-vncclient-macos/VNCRemoteScreenView.swift
@@ -0,0 +1,8 @@
+//
+//  VNCRemoteScreenView.swift
+//  x11-vncclient-macos
+//
+//  Created by Yu Liu on 2024/12/27.
+//
+
+import Foundation
